46b8f70679aaff86eea3adfc1a73146673c2f19c
Arigo.Fahreza <arigo@jkt1.ebdesk.com>
2022-10-24 11:12:25 +0700

    refactor: change update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.service.consume import consume, postgre, sample
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="reprocess data IMA")
-    parser.add_argument("--args", nargs='+', default=[1])
+    parser.add_argument("--args", nargs='+', default=[0])
     args = parser.parse_args()
     postgre(int(vars(args).get('args')[0]))
     # sample()
